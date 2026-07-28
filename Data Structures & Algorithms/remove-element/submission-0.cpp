017e663efb95d3cf77b0a4b398fc72bedc4aab4c
Shreya Kesarwani <128245622+shreyakesri123@users.noreply.github.com>
2026-07-28 20:04:48 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int j=0, i=0;
+        while(i<nums.size())
+        {
+            if(nums[i]!=val)
+            {
+                nums[j++]=nums[i];
+            }
+            i++;
+        }
+
+       return j;
+    }
+};
