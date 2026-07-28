a9facf90d2667dbb8b02d6ca8e9e7f268fcd0d2e
Shreya Kesarwani <128245622+shreyakesri123@users.noreply.github.com>
2026-07-28 20:42:03 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+       string ans;
+       for(auto it:strs)
+       {
+        int n = it.size();
+        ans+=to_string(n)+"#"+ it;
+       }
+       return ans;
+    }
+
+    vector<string> decode(string s) {
+         int n =s.size();
+           vector<string>ans;
+           int a=0;
+           string b;
+           int i=0;
+
+           while(i<s.size())
+           {
+             int j=i;
+             while(j<n && s[j]!='#')
+             {
+                a=a*10+ s[j++]-'0';
+             }
+             j++;
+             b= s.substr(j,a);
+             ans.push_back(b);
+             i= j+a;
+             a=0;
+           }
+           return ans;
+    }
+};
