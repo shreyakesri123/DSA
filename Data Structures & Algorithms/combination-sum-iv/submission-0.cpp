3b3e4ccb0eca51ea52d7145f61b6396b7decda26
Shreya Kesarwani <128245622+shreyakesri123@users.noreply.github.com>
2026-08-10 13:16:02 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int f(int target , vector<int>&nums, vector<int>&dp)
+{
+    if(target<0) return 0;
+    if(target==0) return 1;
+    if(dp[target]!=-1) return dp[target];
+
+    int count =0;
+    for(int i=0;i<nums.size();i++)
+    {
+        count+=f(target-nums[i], nums,dp);
+    }
+    return dp[target]=count;
+}
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        vector<int>dp(target+1,-1);
+        return f(target, nums, dp);
+    }
+};
