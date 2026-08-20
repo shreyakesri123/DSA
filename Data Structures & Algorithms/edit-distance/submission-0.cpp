2cbfcde4cb8d2381cfd12a13ae8b33474250deef
Shreya Kesarwani <128245622+shreyakesri123@users.noreply.github.com>
2026-08-20 18:23:11 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDistance(string s, string t) {
+        
+        int n=s.size(), m= t.size();
+      vector<vector<int>>dp(n+1,vector<int>(m+1));
+      
+       for(int i=0;i<=n;i++)
+      
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0) dp[i][j]=j;
+            if(j==0) dp[i][j]=i;
+        }
+
+      for(int i=1;i<=n;i++)
+      {
+        for(int j=1;j<=m;j++)
+        {
+            if(s[i-1]==t[j-1]) dp[i][j]= dp[i-1][j-1] ;
+            else dp[i][j]= 1+ min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]));
+        }
+      }   
+    return dp[n][m];
+    }
+};
